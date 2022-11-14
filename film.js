5c1263848a7e6d60849b7f677ce81566c4392484
utku <bayraktarogluutku@gmail.com>
2022-11-15 00:35:16 +0300

    film js kontrol

---
+++ film.js
@@ -0,0 +1,10 @@
+// Burada film objemizi tanımlamak için classlarımızı yazacağız
+
+
+//ADIM 1 ********************************************
+function Film (t, director, url){
+    this.title = t
+    this.director =director
+    this.url = url
+}
+
